b2662b9522e0c692bca06789d70288615b783a83
Sakke Laaksonen <sakke.laaksonen@gmail.com>
2014-08-17 22:40:46 +0300

    parsed XML to JSON

---
+++ models/image.rb
@@ -0,0 +1,14 @@
+# class Image < CouchRest::Model::Base
+  
+#   use_database $COUCHDB
+
+#   property :title, String
+#   property :edition, String
+#   property :url, String
+#   # property :year, String
+#   property :dimensions, String
+
+#   design do
+#     view :by_title
+#   end
+# end
