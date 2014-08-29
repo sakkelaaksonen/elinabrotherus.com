4d7b20d36a487e1dc478c7bc897f6f9aea41559b
Sakke Laaksonen <sakke.laaksonen@gmail.com>
2014-08-29 18:42:01 +0300

    stuff

--- app/js/load.js
+++ app/js/load.js
@@ -1,20 +1,21 @@
 head.load([{
-  'jq': 'http://code.jquery.com/jquery-1.11.1.min.js'
-}, {
-  'lazy': 'https://raw.github.com/tuupola/jquery_lazyload/master/jquery.lazyload.js'
-}, {
-  'bs': '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'
-}, {
-  'app': '/js/app.js'
-}, {
-  'cookie': "/js/jquery.cookie.js"
-}, {
-  'scroll': "/js/jquery.scrollTo-1.4.2-min.js"
-}, {
-  'form': "/js/jquery.form.js"
-}, {
-  'gb': "/js/brotherus.guestbook.js"
-}]);
+    'jq': 'http://code.jquery.com/jquery-1.11.1.min.js'
+  }, {
+    'lazy': 'https://raw.github.com/tuupola/jquery_lazyload/master/jquery.lazyload.js'
+  }, {
+    'bs': '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'
+  },
+  // { 'app': '/js/app.js'}, 
+  {
+    'cookie': "/js/jquery.cookie.js"
+  }, {
+    'scroll': "/js/jquery.scrollTo-1.4.2-min.js"
+  }, {
+    'form': "/js/jquery.form.js"
+  }, {
+    'gb': "/js/brotherus.guestbook.js"
+  }
+]);
 
 head.ready(function() {
   //jq onload
