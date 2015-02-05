6a5a857f5fb0ea36eb66b7e8730b9d60b138fe8d
Sakke Laaksonen <sakke.laaksonen@gmail.com>
2015-02-05 17:21:40 +0200

    load lazyload script locally

--- gallery_xml_to_json.rb
+++ gallery_xml_to_json.rb
@@ -1,34 +1,30 @@
 require 'nokogiri'
 require 'json'
-# galleryxml = Nokogiri::XML::parse(File.read('gallerytemp.xml'))
-# puts galleryxml
+galleryxml = Nokogiri::XML::parse(File.read('gallerytemp.xml'))
+puts galleryxml
 
-# galleryhash =  galleryxml.xpath('//gallery').map do |node|
-#   {
-#     # childen: node.xpath('folder').text
-    
-#     pics: node.xpath('pic').each_with_index.map { |pic,i|
-
-#       {
-#         id: "#{(i<10 ? '0':'')}#{i+1}",
-#         #src: "#{node.xpath('folder').text}/#{pic.xpath('lores').text}.jpg",
-#         name: pic.xpath('title').text,
-#         dimensions: pic.xpath('dimensions').text,
-#         year: pic.xpath('year').text,
-#         edition: pic.xpath('editions').text
-#     }}
-#   }
-# end
-
-
+galleryhash =  galleryxml.xpath('//gallery').map do |node|
+  # {
+  #   # childen: node.xpath('folder').text
+  node.xpath('pic').each_with_index.map { |pic,i|
+  {
+    id: "#{(i<10 ? '0':'')}#{i+1}",
+    #src: "#{node.xpath('folder').text}/#{pic.xpath('lores').text}.jpg",
+    name: pic.xpath('title').text,
+    dimensions: pic.xpath('dimensions').text,
+    year: pic.xpath('year').text,
+    edition: ( pic.xpath('editions').text.empty?  ? '6' : pic.xpath('editions').text )
+  }}.flatten
+  # }
+end
 
 # # puts galleryhash.to_json
 # # newjson = JSON.parse(File.read('newfiles.json'))
 # # newjson['galleries'].concat(galleryhash)
 
-g = JSON.parse(File.read('allgalleries.json'))
-g['galleries'].each do |gal|
-    gal['pics'].each { |pic| pic['edition'] = "6" }
-end
+# g = JSON.parse(File.read('allgalleries.json'))
+# gh = g['galleries'].map do |gal|
+#     gal['pics'].each { |pic| pic['edition'] = "6" }
+# end
 
-File.open('allgalleries2.json','w') {|f| f.write(g.to_json)}
+File.open('gallerytemp.json','w') {|f| f.write(galleryhash.to_json)}
--- public/javascripts/load.js
+++ public/javascripts/load.js
@@ -1,7 +1,7 @@
 head.load([{
   'jq': 'http://code.jquery.com/jquery-1.11.1.min.js'
 }, {
-  'lazy': 'https://raw.github.com/tuupola/jquery_lazyload/master/jquery.lazyload.js'
+  'lazy': '/javascripts/jquery.lazyload.js'
 }, {
   'bs': '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'
 }, {
